6529ce4e986e79d969e36bf49fe9e512600e5d2d
edgar <ealejor@fcpn.edu.bo>
2022-09-08 11:06:03 -0400

    update

--- DefenseOne/AplicacionMensajeria.cs
+++ DefenseOne/AplicacionMensajeria.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DefenseOne
 {
     public class AplicacionMensajeria
@@ -8,16 +10,49 @@ namespace DefenseOne
         private int nroContactos;
         private string[,] contactos = new string[50, 2];
 
-        public AplicacionMensajeria(string nom, int nroGrupos, int nroContactos)
+        public AplicacionMensajeria()
         {
-            this.nom = nom;
-            this.nroGrupos = nroGrupos;
-            this.nroContactos = nroContactos;
+            nom = "signal";
+            nroGrupos = 3;
+            grupos[0, 0] = "Lab-121 F"; grupos[0, 1] = "110"; grupos[0, 2] = "2/08/2022"; grupos[0, 3] = "701003";
+            grupos[1, 0] = "wally"; grupos[1, 1] = "22"; grupos[1, 2] = "15/03/2022"; grupos[1, 3] = "605252";
+            grupos[2, 0] = "info"; grupos[2, 1] = "5"; grupos[2, 2] = "15/03/2022"; grupos[2, 3] = "701003";
+            nroContactos = 5;
+            contactos[0, 0] = "rodrigo"; contactos[0, 1] = "601020";
+            contactos[1, 0] = "adrian"; contactos[1, 1] = "706414";
+            contactos[2, 0] = "lupe"; contactos[2, 1] = "605252";
+            contactos[3, 0] = "maría"; contactos[3, 1] = "701003";
+            contactos[4, 0] = "daniel"; contactos[4, 1] = "705555";
         }
 
-        public AplicacionMensajeria(string nom)
+        public AplicacionMensajeria(string nombre)
         {
-            this.nom = nom;
+            nom = nombre;
+            nroGrupos = 2;
+            grupos[0, 0] = "Arduino"; grupos[0, 1] = "10"; grupos[0, 2] = "22/01/2022"; grupos[0, 3] = "123456";
+            grupos[1, 0] = "full joda"; grupos[1, 1] = "8"; grupos[1, 2] = "23/04/2022"; grupos[1, 3] = "605252";
+            nroContactos = 3;
+            contactos[0, 0] = "rodrigo"; contactos[0, 1] = "601020";
+            contactos[1, 0] = "josé"; contactos[1, 1] = "455321";
+        }
+
+        public void mostrar()
+        {
+            Console.WriteLine("Aplicación: " + nom);
+            Console.WriteLine("Número de grupos: " + nroGrupos);
+            Console.WriteLine("-------------------------------------------------------------------------");
+            for (int i = 0; i < nroGrupos; i++)
+            {
+                Console.WriteLine("grupo:" + grupos[i,0] + "    participantes:" + grupos[i,1] + "    creación:" + grupos[i,2] + "    admin:" + grupos[i,3]);
+            }
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("Número de contactos: " + nroContactos);
+            Console.WriteLine("-------------------------------------");
+            for (int i = 0; i < nroContactos; i++)
+            {
+                Console.WriteLine("nombre:" + contactos[i,0] + "    celular:" + contactos[i,1]);
+            }
+            Console.WriteLine("-------------------------------------");
         }
     }
 }
--- DefenseOne/Program.cs
+++ DefenseOne/Program.cs
@@ -6,7 +6,16 @@ namespace DefenseOne
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World malo!");
+            Console.WriteLine("a) █████████████████ Crear dos objetos de mensajeria...");
+            AplicacionMensajeria telegram = new AplicacionMensajeria("telegram");
+            AplicacionMensajeria signal = new AplicacionMensajeria();
+            telegram.mostrar();
+            Console.WriteLine();
+            signal.mostrar();
+            
+            Console.WriteLine("\nb) █████████████████ Dado una apliación de mensajeria...");
+            
+            Console.WriteLine("\nc) █████████████████ Mostrar los nombres de contactos...");
         }
     }
 }
