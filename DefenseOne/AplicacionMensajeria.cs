9ac71b3071af196f59f0e10006359bf5bbbe432e
edgar <ealejor@fcpn.edu.bo>
2022-09-08 10:34:05 -0400

    update

--- DefenseOne/AplicacionMensajeria.cs
+++ DefenseOne/AplicacionMensajeria.cs
@@ -2,6 +2,22 @@ namespace DefenseOne
 {
     public class AplicacionMensajeria
     {
-        
+        private string nom;
+        private int nroGrupos;
+        private string[,] grupos = new string[50, 4];
+        private int nroContactos;
+        private string[,] contactos = new string[50, 2];
+
+        public AplicacionMensajeria(string nom, int nroGrupos, int nroContactos)
+        {
+            this.nom = nom;
+            this.nroGrupos = nroGrupos;
+            this.nroContactos = nroContactos;
+        }
+
+        public AplicacionMensajeria(string nom)
+        {
+            this.nom = nom;
+        }
     }
 }
